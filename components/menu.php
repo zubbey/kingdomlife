5272300e1647df431d1928314160f782ca7e1fd0
zubbey <com.zubbey@icloud.com>
2019-11-22 23:59:06 +0100

    initial Commit

---
+++ components/menu.php
@@ -0,0 +1,13 @@
+<?php ?>
+<!--#### IF LOGIN ######################-->
+<!--//if (isset($_SESSION['user_session'])){-->
+<!--// for logged users-->
+<!--//} else{-->
+<!doctype html>
+<html>
+<head>
+    <title>Home | Kingdom Life Church</title>
+</head>
+<body>
+<div id="name"></div>
+<!--//}-->
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include_once("./config/db.php");
+include ("./components/menu.php");
+include ("./components/header.php");
+include ("./components/main.php");
+include ("./components/footer.php");
