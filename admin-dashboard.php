7334cbcfc115ed1cafd26b5680838b6b4d18d5a8
zubbey <com.zubbey@hotmail.com>
2019-12-12 15:16:35 +0100

    adding audios from admin1

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -3,7 +3,7 @@ require_once("./controller/auth_controller.php");
 require ("./components/menu.php");
 ?>
 <div class="container mt-5">
-    <form   class="md-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method='post' enctype="multipart/form-data">
+    <form   class="md-form" action="audio-upload.php" method='post' enctype="multipart/form-data">
         <div class="file-field">
         <label for="inputDescription">Title: </label>
         <input name="description_entered" type="text" id="inputDescription" class="form-control mb-2" aria-describedby="passwordHelpBlock">
@@ -34,32 +34,6 @@ require ("./components/menu.php");
         </div>
     </div>
 </div>
-<?php
-if(session_id() != '') session_destroy();
-if(isset($_GET['err'])){
-    if($_GET['err'] == 'bf'){
-        $errorMsg = 'Please select a video file for upload.';
-    }elseif($_GET['err'] == 'ue'){
-        $errorMsg = 'Sorry, there was an error uploading your file.';
-    }elseif($_GET['err'] == 'fe'){
-        $errorMsg = 'Sorry, only MP4, AVI, MPEG, MPG, MOV & WMV files are allowed.';
-    }else{
-        $errorMsg = 'Some problems occured, please try again.';
-    }
-}
-?>
-<!---->
-<!--    <div class="youtube-box">-->
-<!--        <h1>Upload video to YouTube using PHP</h1>-->
-<!--        <form method="post" name="multiple_upload_form" id="multiple_upload_form" enctype="multipart/form-data" action="youtube_upload.php">-->
-<!--            --><?php //echo (!empty($errorMsg))?'<p class="err-msg">'.$errorMsg.'</p>':''; ?>
-<!--            <label for="title">Title:</label><input type="text" name="title" id="title" value="" />-->
-<!--            <label for="description">Description:</label> <textarea name="description" id="description" cols="20" rows="2" ></textarea>-->
-<!--            <label for="tags">Tags:</label> <input type="text" name="tags" id="tags" value="" />-->
-<!--            <label for="video_file">Choose Video File:</label>	<input type="file" name="videoFile" id="videoFile" >-->
-<!--            <input name="videoSubmit" id="submit" type="submit" value="Upload">-->
-<!--        </form>-->
-<!--    </div>-->
 <?php
 require ("./components/footer.php");
 ?>
--- controller/auth_controller.php
+++ controller/auth_controller.php
@@ -154,38 +154,3 @@ if (isset($_POST['login-btn'])) {
         }
     }
 }
-
-//UPLOAD AUDIO FILE
-$name= $_FILES['file']['name'];
-
-$tmp_name= $_FILES['file']['tmp_name'];
-
-$submitbutton= $_POST['submit'];
-
-$position= strpos($name, ".");
-
-$fileextension= substr($name, $position + 1);
-
-$fileextension= strtolower($fileextension);
-
-$description= $_POST['description_entered'];
-
-if (isset($name)) {
-
-    $path= 'audio/';
-
-    if (!empty($name)){
-        if (move_uploaded_file($tmp_name, $path.$name)) {
-            header("Location: admin-dashboard?uploaded=true");
-        } else{
-            header("Location: admin-dashboard?uploaded=false");
-        }
-    }
-}
-
-if(!empty($description)){
-    mysqli_query($conn, "INSERT INTO audio (description, filename)
-VALUES ('$description', '$name')");
-}
-
-mysqli_close($conn);
