46ce5b09bc6be32d1f092d546400ccffe9ce973d
zubbey <com.zubbey@hotmail.com>
2019-12-12 15:08:04 +0100

    adding audios from admin

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -3,7 +3,7 @@ require_once("./controller/auth_controller.php");
 require ("./components/menu.php");
 ?>
 <div class="container mt-5">
-    <form   class="md-form" action="audio-upload.php" method='post' enctype="multipart/form-data">
+    <form   class="md-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method='post' enctype="multipart/form-data">
         <div class="file-field">
         <label for="inputDescription">Title: </label>
         <input name="description_entered" type="text" id="inputDescription" class="form-control mb-2" aria-describedby="passwordHelpBlock">
@@ -12,28 +12,28 @@ require ("./components/menu.php");
         </div>
     </form>
 
-<hr>
-<div class="row mt-3">
-    <div class="col">
-        <?php
-        $result= mysqli_query($conn, "SELECT description, filename FROM audio ORDER BY ID desc" )
-        or die("SELECT Error: ".mysqli_error());
+    <hr>
+    <div class="row mt-3">
+        <div class="col">
+            <?php
+            $result= mysqli_query($conn, "SELECT description, filename FROM audio ORDER BY ID desc" )
+            or die("SELECT Error: ".mysqli_error());
 
-        print "<ul class='list-group-item'>";
-        while ($row = mysqli_fetch_array($result)){
-            $files_field= $row['filename'];
-            $files_show= "audio/$files_field";
-            $descriptionvalue= $row['description'];
-            print "<li class='d-flex align-items-center justify-content-between'>";
-            echo "<a href='".$files_show."'>$descriptionvalue</a> <span class='badge badge-success'>Uploaded:</span>";
-            print "</li>";
-        }
-        print "</ul>";
+            echo "<ul class='list-group-item'>";
+            while ($row = mysqli_fetch_array($result)){
+                $files_field= $row['filename'];
+                $files_show= "audio/$files_field";
+                $descriptionvalue= $row['description'];
+                echo "<li class='d-flex align-items-center justify-content-between'>";
+                echo "<a href='".$files_show."'>$descriptionvalue</a> <span class='badge badge-success'>Uploaded:</span>";
+                echo "</li>";
+            }
+            echo "</ul>";
 
-        ?>
+            ?>
+        </div>
     </div>
 </div>
-    </div>
 <?php
 if(session_id() != '') session_destroy();
 if(isset($_GET['err'])){
