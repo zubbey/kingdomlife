656af8d01232018918b31e573a10d35736d5cb4b
zubbey <com.zubbey@hotmail.com>
2020-01-24 10:38:20 +0100

    clearing all data

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("./controller/auth_controller.php");
-require("./components/menu.php");
-require("./components/header.php");
-require("./components/main.php");
-require("./components/footer.php");
