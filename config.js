8032e9e6d458c261a5d98892c3954ad07407d722
zubbey <com.zubbey@hotmail.com>
2020-01-25 10:56:29 +0100

    Correction(4) Made

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    GOOGLE_KEY : 'AIzaSyD2EAYEm2s0Y47rOb77dNPwdh6YhKYbUVo',
+    KINGDOMLIFE_KEY : 'AIzaSyCCorS11q7oJIspTsTZ6OvMv59RWIjinwE',
+    PAYSTACK_PUBLIC_KEY : 'pk_live_3f38bcf11ebcc575162fe2e177cea7b114f2c5db'
+};
