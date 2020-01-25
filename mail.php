7c0fba0bc26072722cda4a0d9ce2561e1e39d519
zubbey <56047720+zubbey@users.noreply.github.com>
2020-01-25 18:19:34 +0100

    testing mail 2

--- mail.php
+++ mail.php
@@ -1,9 +1,38 @@
 <?php
-$to = "com.zubbey@hotmail.com";
-$subject = "Testing From Hosted";
-$txt = "Hello world!";
-$headers = "From: zubbey@protonmail.com" . "\r\n" .
-"CC: zubyinnocent@outlook.com";
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-mail($to,$subject,$txt,$headers);
-?>
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'zubbey@protonmail.com';                     // SMTP username
+    $mail->Password   = '070687';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('no=reply@kingdomlifegospel.org', 'Kingdomlife');
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Testing Kingdomlife Mailing System Test 0.1';
+    $mail->Body    = 'This is the HTML message body <b>Hello World!</b>';
+    $mail->addAddress('com.zubbey@hotmail.com', 'Zubbey');     // Add a recipient
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
