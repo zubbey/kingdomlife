b176750815a5ed069d75c68744305a6514afbc09
zubbey <56047720+zubbey@users.noreply.github.com>
2020-01-26 03:14:42 +0100

    removed error on emailcontroller

--- controller/emailController.php
+++ controller/emailController.php
@@ -472,7 +472,7 @@ function sendVerificationEmail($userEmail, $token){
         $mail->send();
 
     } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+//         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
 
@@ -952,7 +952,7 @@ function resetpasswordMail($email, $token){
         $mail->send();
 
     } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+//         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
 
